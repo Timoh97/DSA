bc63c9112fcc0b2f38911cdf80e7f413d00f11e5
Timoh97 <timohmugendi@gmail.com>
2023-03-30 22:49:31 +0300

    comment the code

--- insertionSort.js
+++ insertionSort.js
@@ -1,19 +1,24 @@
 function insertionSort (arr) 
 {
+    // check if arr has one element
     if (arr.length <=1){
         return arr
     }
+    //set for loop and initialize key and i =1
+    // initialize j also
+    // the assumption is arr[0] is sorted
     for (let i=1; i<arr.length; i++)
-    key = arr[i];
-    var j = i-1;
+        var key = arr[i];
+        var j = i-1;
 
         while (j >= 0 && arr[j] > arr[i]){
+            // if the condition is satisfied swap
             arr[j+1] = arr[j]
             j= j-1
     }
     arr[j+1] = key
 return arr
-    }
+}
 
 
 
