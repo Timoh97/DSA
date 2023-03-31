c0ceba6849c70404009255b9a4d0cb2a96162e45
Timoh97 <timohmugendi@gmail.com>
2023-03-31 13:50:39 +0300

    add when empty code and when not empty

--- linkedlist/singlylinked/sLinkedlist.js
+++ linkedlist/singlylinked/sLinkedlist.js
@@ -25,4 +25,28 @@ class Linkedlist {
 
         return this.size
     }
+
+    //adding a value to node
+    // method accepts a value
+    prepend (value){
+        const node = new Node (value)
+
+        // Case 1: list is Empty
+        if (this.isEmpty()) {
+            this.head = node
+        }
+        // Case 2: list is not empty
+        else {
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+        
+
+        
+
+    }
 }
+
+
+// code refactor
