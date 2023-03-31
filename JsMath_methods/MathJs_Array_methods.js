f5789c66e0b130f5a91d5009153e8eeb11314363
Timoh97 <timohmugendi@gmail.com>
2023-04-01 02:32:51 +0300

    add most common math array methods in javascript

---
+++ JsMath_methods/MathJs_Array_methods.js
@@ -0,0 +1,37 @@
+//Javascript Math Cheat Sheet
+//Javascript math object provides several constants and methods to perform mathematical operations
+
+
+// These are the important js methods, REMEMBER THEM!!
+
+var pi = Math.PI // pi constant, 3.14159265358
+
+Math.round(4.4); // 4 rounded
+
+Math.round(4.5); // 5- round up
+
+Math.pow(2,8); // 256, 2 raised to power 8
+
+Math.sqrt(49); // 7, square root of 49
+
+Math.abs(-3.14); //3.14, absolute/ positive value
+
+Math.ceil(3.14); // 4, it's rounded up
+
+Math.floor(3.99); // 3, rounded down
+
+Math.sin(0); // 0, sine of 0 = 0
+
+Math.cos(Math.PI); // -1, the cosine
+
+Math.log(1); // 0, natural log of 0 = 0
+
+Math.max(0, 3, -2, 2); // 3
+
+Math.min (0, 3, -2, 2); // -2
+
+Math.exp(1); // 2.7182818, e to the power of x
+
+Math.random() // generates any number from 0 to 1 randomly
+
+Math.floor(Math.random()*5) + 1  // random integer 1 to 5
