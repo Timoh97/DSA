e26d401c8ce298b862724242f053a5bd0dfb4bd9
Timoh97 <timohmugendi@gmail.com>
2023-04-05 10:25:29 +0300

    array methods

--- JS_Array_methods/commonArrays/arraymethods.js
+++ JS_Array_methods/commonArrays/arraymethods.js
@@ -27,3 +27,35 @@ entries() //Returns a key/value pair Array Iteration Object
 copyWithin() //Copies array elements within the array, to and from specified positions
 
 constructor ()// Returns the function that created the Array object's prototype
+
+unshift() // Adds new elements to the beginning of an array, and returns the new length
+
+valueOf() //Returns the primitive value of an array
+
+toString() //Converts an array to a string, and returns the result
+
+splice()// sort()
+
+some() // Checks if any of the elements in an array pass a test
+
+slice() //Selects a part of an array, and returns the new array
+
+shift() // Removes the first element of an array, and returns that element
+
+reverse() // Reverses the order of the elements in an array
+
+reduceRight() // Reduce the values of an array to a single value (going right-to-left)
+
+reduce() // Reduce the values of an array to a single value (going left-to-right)
+
+push() //Adds new elements to the end of an array, and returns the new length
+
+pop()// Removes the last element of an array, and returns that element
+
+map() // Creates a new array with the result of calling a function for each array element
+
+length // Sets or returns the number of elements in an array
+
+lastIndexOf() // Search the array for an element, starting at the end, and returns its position
+
+keys() //Returns a Array Iteration Object, containing the keys of the original array
