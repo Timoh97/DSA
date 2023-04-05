6f398576efbef5a1c93b0655d655118f86ceae8b
Timoh97 <timohmugendi@gmail.com>
2023-04-05 12:48:35 +0300

    get date settings

---
+++ JS_Array_methods/dateMethods/date_time_year.js
@@ -0,0 +1,43 @@
+// get date
+
+getDate () // The getDate() method returns the day of the month (1 to 31) of a date
+var d = new Date();
+let date = d.getDate();
+
+getDay() // The getDay() method returns the day of the week (0 to 6) of a date.
+const d = new Date();
+let day = d.getDay();
+
+getFullyear () // getFullYear() returns the full year (4 digits) of a date.
+const d = new Date();
+let year = d.getFullyear ();
+
+getHours () //  getHours() returns the hour (0 to 23) of a date.
+const d = new Date();
+let hours = d.getHours ()
+
+getMinutes() // getMinutes() returns the minutes (0 to 59) of a date.
+const d = new Date();
+let minutes = d.getMinutes() 
+
+getMilliseconds () // getMilliseconds() returns the milliseconds (0 to 999) of a dat
+const d = new Date();
+let milliseconds = d.getMilliseconds () 
+
+getSeconds () // getSeconds() returns the seconds (0 to 59) of a date
+const d = new Date();
+let seconds = d.getSeconds ()
+
+getMonth () // getMonth() returns the month (0 to 11) of a date
+const d = new Date();
+
+getTime () // getTime() returns the number of milliseconds since January 1, 1970 00:00:00.
+const d = new Date();
+let time = d.getMonth ();
+
+getUTCDate () // getUTCDate() returns the day of the month (1 to 31) of a date object
+const d = new Date();
+let utcdate = d.getUTCDate ();
+
+
+// set date and time
