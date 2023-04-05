703061a6aa0733257529b919886e22007090dd4d
Timoh97 <timohmugendi@gmail.com>
2023-04-05 10:16:45 +0300

    array methods

---
+++ JS_Array_methods/commonArrays/arraymethods.js
@@ -0,0 +1,29 @@
+concat() // Joins arrays and returns an array with the joined arrays
+
+isArray() // Checks whether an object is an array
+
+join() // Joins all elements of an array into a string
+
+indexOf() //Search the array for an element and returns its position
+
+includes() // Check if an array contains the specified element
+
+from() //Creates an array from an object
+
+forEach() //Calls a function for each array element
+
+findIndex() // Returns the index of the first element in an array that pass a test
+
+find() //Returns the value of the first element in an array that pass a test
+
+filter() //Creates a new array with every element in an array that pass a test
+
+fill() //Fill the elements in an array with a static value
+
+every() //Checks if every element in an array pass a test
+
+entries() //Returns a key/value pair Array Iteration Object
+
+copyWithin() //Copies array elements within the array, to and from specified positions
+
+constructor ()// Returns the function that created the Array object's prototype
