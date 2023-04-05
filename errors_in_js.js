f5c70ccee5301723efe2dd58e6226a4ffd0372ba
Timoh97 <timohmugendi@gmail.com>
2023-04-05 10:46:13 +0300

    errors

---
+++ errors_in_js.js
@@ -0,0 +1,14 @@
+try // defines a block of code totest for errors
+
+catch //set up a block of code to execute in case of an error
+
+throw //creates custom error messages
+
+finally //lets the code execute after trying try and catch regardless of the result
+
+try {
+    Block of code to try
+}
+catch(err) {
+    Block of code to handle errors
+}
