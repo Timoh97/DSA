6b05a62c2f701d0998897d0f0669995cd7ede01a
Timoh97 <timohmugendi@gmail.com>
2023-04-06 00:45:40 +0300

    write about big o-notation, and explain using comments

---
+++ 0x0A-intro/Big_0_notation/Big_0_notation.js
@@ -0,0 +1,48 @@
+// Different types of time complexities
+
+
+//1 constant time complexity - O(1)
+
+        // this complexity is not dependent on input size and the runtime will always be the same
+
+//2. linear time complexity - O(n)
+
+        // for this complexity, runtime will be directly proportional to length of the input
+
+//3. logarithmic time complexity - O( log n)
+
+        // an algorithm has logarithmic time complexity when;
+        // it reduces size of the input data in each step
+        // i.e number of operations decrease as input size decrease
+
+//4. quadratic time complexity - O( n ** 2)
+
+        // has non-linear time complexity where the run-time increases non-linearly (O(N**2)) with length of output
+        // nested loops take this order
+
+//5. cubic time complexity - O( n ** 3)
+
+
+
+
+// SEARCH ALGORITHMS
+
+//LINEAR SEARCH =       best case O(1)                            worst case        O(n) 
+
+
+//BINARY SEARCH =       best case O(1)                            worst case        O(log n)
+
+
+// SORTING ALGORITHMS
+
+// INSERTION SORT =      best case O(n)                            worst case        O(n ** 2) 
+
+// MERGE SORT     =      best case O(n log n)                      worst case        O(n log n) 
+
+// BUBBLE SORT    =      best case O(n)                            worst case        O(n ** 2) 
+
+// QUICK SORT     =      best case O(n log n)                      worst case        O(n ** 2) 
+
+
+
+// NB. QUICK SORT - is the best due to its performance and because it is the fastest of all the sorting algorithms
